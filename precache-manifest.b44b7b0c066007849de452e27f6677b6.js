4f279a3fd3faf38048d6e7a18b76fd5b1e90f19a
SergProgMan <semibratyev@gmail.com>
2020-01-26 20:40:55 +0200

    Updates

---
+++ precache-manifest.b44b7b0c066007849de452e27f6677b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87712f7fd86ded73457f3850d1ac6fc",
+    "url": "/react-crwn-clothing/index.html"
+  },
+  {
+    "revision": "358ac24603196164ed08",
+    "url": "/react-crwn-clothing/static/css/main.8f799647.chunk.css"
+  },
+  {
+    "revision": "e1f1e7fc2c914d77883b",
+    "url": "/react-crwn-clothing/static/js/2.635389b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-crwn-clothing/static/js/2.635389b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "358ac24603196164ed08",
+    "url": "/react-crwn-clothing/static/js/main.da793f52.chunk.js"
+  },
+  {
+    "revision": "001787e7087667ac348a",
+    "url": "/react-crwn-clothing/static/js/runtime-main.9e606931.js"
+  }
+]);
